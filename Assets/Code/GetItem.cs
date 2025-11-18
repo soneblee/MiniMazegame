f293100862ccb9950559efdac7035eb2da009279
unbany <130903252+soneblee@users.noreply.github.com>
2025-11-18 21:48:21 +0900

    아이템  획득 시 효과음 추가

--- Assets/Code/GetItem.cs
+++ Assets/Code/GetItem.cs
@@ -6,7 +6,7 @@ public class GetItem : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
         ItemSound.GetItemSound();
+        Destroy(gameObject);
     }
 }
